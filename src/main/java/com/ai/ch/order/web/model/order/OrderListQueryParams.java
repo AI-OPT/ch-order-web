d36d2d76ebb66fb5d61963af59c7b0c589dd24d7
fazhicao <409993207@qq.com>
2016-09-01 16:30:31 +0800

    订单列表查询接受参数

---
+++ src/main/java/com/ai/ch/order/web/model/order/OrderListQueryParams.java
@@ -0,0 +1,35 @@
+package com.ai.ch.order.web.model.order;
+
+import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
+
+public class OrderListQueryParams extends BehindQueryOrderListRequest{
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 订单状态
+	 */
+	private String states;
+	
+	/**
+	 * 用户名
+	 */
+	private String username;
+
+	public String getStates() {
+		return states;
+	}
+
+	public void setStates(String states) {
+		this.states = states;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
