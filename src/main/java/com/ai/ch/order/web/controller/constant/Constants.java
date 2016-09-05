c8283e4a525890505f53c139342e615e8cfcba22
fazhicao <409993207@qq.com>
2016-09-05 14:11:24 +0800

    修改积分状态

--- src/main/java/com/ai/ch/order/web/controller/constant/Constants.java
+++ src/main/java/com/ai/ch/order/web/controller/constant/Constants.java
@@ -22,6 +22,55 @@ public final class Constants {
 	public static final String ORD_LOGISTICS_TYPE = "ORD_LOGISTICS_TYPE";
 	/** 订单来源*/
 	public static final String ORD_CHL_ID = "CHL_ID";
-	 
-}
+	
+	
+	public static final class OrdOrder {
 
+		public static class State {
+ 
+            /**
+             * 11 待付款
+             */
+            public static final String WAIT_PAY = "11";
+
+            /**
+             * 13 待配货
+             */
+            public static final String WAIT_DISTRIBUTION="13";
+            
+            /**
+             * 14 待出库
+             */
+            public static final String WAIT_DELIVERY = "14";
+            
+            /**
+             * 15 待发货
+             */
+            public static final String WAIT_SEND="15";
+ 
+            /**
+             * 16 待确认
+             */
+            public static final String WAIT_CONFIRM = "16";
+
+            /**
+             * 90 完成
+             */
+            public static final String COMPLETED = "90";
+
+            /**
+             * 91取消
+             */
+            public static final String CANCEL = "91";
+        }
+		
+		public static class PayStyle {
+			
+			/**
+			 * 积分支付
+			 */
+			public static final String JF = "5";
+		}
+	}
+
+}
