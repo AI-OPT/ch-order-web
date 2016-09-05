67210bbe32f7072f6bbc6248dfa8aa8695dd7d6d
fazhicao <409993207@qq.com>
2016-09-05 13:58:38 +0800

    订单列表返回修改

--- src/main/java/com/ai/ch/order/web/controller/order/OrderListController.java
+++ src/main/java/com/ai/ch/order/web/controller/order/OrderListController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.ai.ch.order.web.controller.constant.Constants;
 import com.ai.ch.order.web.model.ProductVo;
+import com.ai.ch.order.web.model.order.OrdOrderListVo;
 import com.ai.ch.order.web.model.order.OrderDetail;
 import com.ai.ch.order.web.model.order.OrderListQueryParams;
 import com.ai.ch.order.web.model.sso.client.GeneralSSOClientUser;
@@ -29,6 +30,7 @@ import com.ai.opt.sdk.util.BeanUtils;
 import com.ai.opt.sdk.web.model.ResponseData;
 import com.ai.opt.sso.client.filter.SSOClientConstants;
 import com.ai.slp.order.api.orderlist.interfaces.IOrderListSV;
+import com.ai.slp.order.api.orderlist.param.BehindOrdOrderVo;
 import com.ai.slp.order.api.orderlist.param.BehindParentOrdOrderVo;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListRequest;
 import com.ai.slp.order.api.orderlist.param.BehindQueryOrderListResponse;
@@ -54,8 +56,8 @@ public class OrderListController {
      */
     @RequestMapping("/getOrderListData")
     @ResponseBody
-    public ResponseData<PageInfo<BehindParentOrdOrderVo>> getList(HttpServletRequest request,OrderListQueryParams queryParams){
-    	ResponseData<PageInfo<BehindParentOrdOrderVo>> responseData = null;
+    public ResponseData<PageInfo<OrdOrderListVo>> getList(HttpServletRequest request,OrderListQueryParams queryParams){
+    	ResponseData<PageInfo<OrdOrderListVo>> responseData = null;
 	    try{
 	    	BehindQueryOrderListRequest queryRequest = new BehindQueryOrderListRequest();
 			BeanUtils.copyProperties(queryRequest, queryParams);
@@ -80,20 +82,35 @@ public class OrderListController {
 			}else{
 				queryRequest.setOrderTimeEnd(null);
 			}
-			queryRequest.setTenantId("SLP");
+			queryRequest.setTenantId(Constants.TENANT_ID);
 			//用户信息  根据username获取用户id??
 			IOrderListSV iOrderListSV = DubboConsumerFactory.getService(IOrderListSV.class);
 			BehindQueryOrderListResponse orderListResponse = iOrderListSV.behindQueryOrderList(queryRequest);
+			PageInfo<OrdOrderListVo> pageInfoVo = new PageInfo<OrdOrderListVo>();
 			if (orderListResponse != null && orderListResponse.getResponseHeader().isSuccess()) {
 				PageInfo<BehindParentOrdOrderVo> pageInfo = orderListResponse.getPageInfo();
-				responseData = new ResponseData<PageInfo<BehindParentOrdOrderVo>>(ResponseData.AJAX_STATUS_SUCCESS, "查询成功", pageInfo);
+				BeanUtils.copyProperties(pageInfoVo, pageInfo);
+				List<BehindParentOrdOrderVo> result = pageInfo.getResult();
+				List<OrdOrderListVo> orderList=new ArrayList<OrdOrderListVo>();
+				if(!CollectionUtil.isEmpty(result)) {
+					for (BehindParentOrdOrderVo behindParentOrdOrderVo : result) {
+						OrdOrderListVo orderListVo=new OrdOrderListVo();
+						BeanUtils.copyProperties(orderListVo, behindParentOrdOrderVo);
+						orderListVo.setTotalAdjustFee(AmountUtil.LiToYuan(behindParentOrdOrderVo.getAdjustFee()));
+						orderListVo.setTotalDiscountFee(AmountUtil.LiToYuan(behindParentOrdOrderVo.getDiscountFee()));
+						orderListVo.setTotalJF(AmountUtil.LiToYuan(behindParentOrdOrderVo.getPoints()));
+						orderList.add(orderListVo);
+					}
+				}
+				pageInfoVo.setResult(orderList);
+				responseData = new ResponseData<PageInfo<OrdOrderListVo>>(ResponseData.AJAX_STATUS_SUCCESS, "查询成功",pageInfoVo);
 			} else {
-				responseData = new ResponseData<PageInfo<BehindParentOrdOrderVo>>(ResponseData.AJAX_STATUS_FAILURE, "查询失败", null);
+				responseData = new ResponseData<PageInfo<OrdOrderListVo>>(ResponseData.AJAX_STATUS_FAILURE, "查询失败", null);
 			}
 		} catch (Exception e) {
 			logger.error("查询订单列表失败：", e);
 			e.printStackTrace();
-			responseData = new ResponseData<PageInfo<BehindParentOrdOrderVo>>(ResponseData.AJAX_STATUS_FAILURE, "查询失败", null);
+			responseData = new ResponseData<PageInfo<OrdOrderListVo>>(ResponseData.AJAX_STATUS_FAILURE, "查询失败", null);
 		}
 	    return responseData;
     }
