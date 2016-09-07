0734d399b62abfc3dac61de25f4200d035ffd33c
fazhicao <409993207@qq.com>
2016-09-07 16:02:49 +0800

    租户id修改

--- src/main/java/com/ai/ch/order/web/controller/order/DeliveryOrderPrintController.java
+++ src/main/java/com/ai/ch/order/web/controller/order/DeliveryOrderPrintController.java
@@ -3,7 +3,6 @@ package com.ai.ch.order.web.controller.order;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ai.ch.order.web.model.sso.client.GeneralSSOClientUser;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.ai.opt.sdk.web.model.ResponseData;
-import com.ai.opt.sso.client.filter.SLPClientUser;
 import com.ai.opt.sso.client.filter.SSOClientConstants;
 import com.ai.slp.order.api.deliveryorderprint.interfaces.IDeliveryOrderPrintSV;
 import com.ai.slp.order.api.deliveryorderprint.param.DeliveryOrderPrintInfosRequest;
@@ -35,15 +34,14 @@ public class DeliveryOrderPrintController {
 	@ResponseBody
 	public ResponseData<DeliveryOrderQueryResponse> query(HttpServletRequest request,String orderId) {
 		ResponseData<DeliveryOrderQueryResponse> responseData =null;
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		try {
 			DeliveryOrderPrintRequest req=new DeliveryOrderPrintRequest();
-			/*	 SLPClientUser user = (SLPClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
-			if (user==null)
-			    throw new BusinessException("","请先登录");
-			*/
-			req.setOrderId(35913355l);
+			req.setOrderId(Long.parseLong(orderId));
+			//req.setUserId(user.getUserId());
 			req.setUserId("000000000000000945");
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			IDeliveryOrderPrintSV iDeliveryOrderPrintSV = DubboConsumerFactory.getService(IDeliveryOrderPrintSV.class);
 			DeliveryOrderQueryResponse response = iDeliveryOrderPrintSV.query(req);
 			if(response!=null && response.getResponseHeader().isSuccess()) {
@@ -63,17 +61,16 @@ public class DeliveryOrderPrintController {
 	@RequestMapping("/noMergeQuery")
 	@ResponseBody
 	public ResponseData<DeliveryOrderPrintResponse> noMergeQuery(HttpServletRequest request,String orderId) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<DeliveryOrderPrintResponse> responseData =null;
 		try{
 			IDeliveryOrderPrintSV iDeliveryOrderPrintSV = DubboConsumerFactory.getService(IDeliveryOrderPrintSV.class);
 			DeliveryOrderPrintRequest req=new DeliveryOrderPrintRequest();
-			/*	 SLPClientUser user = (SLPClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
-			if (user==null)
-			    throw new BusinessException("","请先登录");
-			*/
-			req.setOrderId(35913355l);
+			req.setOrderId(Long.parseLong(orderId));
+			//req.setUserId(user.getUserId());
 			req.setUserId("000000000000000945");
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			DeliveryOrderPrintResponse response = iDeliveryOrderPrintSV.noMergePrint(req);
 			if(response!=null && response.getResponseHeader().isSuccess()) {
 				responseData = new ResponseData<DeliveryOrderPrintResponse>(ResponseData.AJAX_STATUS_SUCCESS, "提货单不合并查询成功",response);
@@ -93,18 +90,15 @@ public class DeliveryOrderPrintController {
 	@RequestMapping("/display")
 	@ResponseBody
 	public ResponseData<DeliveryOrderPrintResponse> display(HttpServletRequest request,String orderId) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<DeliveryOrderPrintResponse> responseData =null;
 	    try {
-		//	HttpSession session = request.getSession();
 			DeliveryOrderPrintRequest req=new DeliveryOrderPrintRequest();
-		/*	 SLPClientUser user = (SLPClientUser) session
-			        .getAttribute(SSOClientConstants.USER_SESSION_KEY);
-			if (user==null)
-			    throw new BusinessException("","请先登录");
-			*/
-			req.setOrderId(35913355l);
+			req.setOrderId(Long.parseLong(orderId));
+			//req.setUserId(user.getUserId());
 			req.setUserId("000000000000000945");
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			IDeliveryOrderPrintSV iDeliveryOrderPrintSV = DubboConsumerFactory.getService(IDeliveryOrderPrintSV.class);
 			DeliveryOrderPrintResponse response = iDeliveryOrderPrintSV.display(req);
 			if(response.getResponseHeader().isSuccess()) {
@@ -125,19 +119,16 @@ public class DeliveryOrderPrintController {
 	@ResponseBody
 	public ResponseData<Boolean> print(HttpServletRequest request,String orderId,
 			String contactName,String orderInfos) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<Boolean> responseData =null;
 	    try {
-			//HttpSession session = request.getSession();
 			DeliveryOrderPrintInfosRequest req=new DeliveryOrderPrintInfosRequest();
-			/* SLPClientUser user = (SLPClientUser) session
-			        .getAttribute(SSOClientConstants.USER_SESSION_KEY);
-			if (user==null)
-			    throw new BusinessException("","请先登录");*/
 			List<DeliveryProdPrintVo> vos = JSON.parseArray(orderInfos, DeliveryProdPrintVo.class); 
 			req.setOrderId(Long.valueOf(orderId));
 			req.setContactName(contactName);
 			req.setDeliveryProdPrintVos(vos);
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			IDeliveryOrderPrintSV iDeliveryOrderPrintSV = DubboConsumerFactory.getService(IDeliveryOrderPrintSV.class);
 			BaseResponse response = iDeliveryOrderPrintSV.print(req);
 			if(response.getResponseHeader().isSuccess()) {
