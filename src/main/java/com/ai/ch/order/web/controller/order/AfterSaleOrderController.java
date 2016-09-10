141be430555eae9375d4576289d972f86a77c1fd
fazhicao <409993207@qq.com>
2016-09-10 10:03:54 +0800

    获取登录用户

--- src/main/java/com/ai/ch/order/web/controller/order/AfterSaleOrderController.java
+++ src/main/java/com/ai/ch/order/web/controller/order/AfterSaleOrderController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ai.ch.order.web.model.sso.client.GeneralSSOClientUser;
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.ai.opt.sdk.web.model.ResponseData;
+import com.ai.opt.sso.client.filter.SSOClientConstants;
 import com.ai.slp.order.api.aftersaleorder.interfaces.IOrderAfterSaleSV;
 import com.ai.slp.order.api.aftersaleorder.param.OrderReturnRequest;
 
@@ -22,23 +24,27 @@ public class AfterSaleOrderController {
 	
 	@RequestMapping("/back")
 	@ResponseBody
-	public ResponseData<String> back(HttpServletRequest request, String orderId,String prodDetalId) {
+	public ResponseData<String> back(HttpServletRequest request, String orderId,
+			String prodDetalId,String prodSum) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<String> data=null;
 		try {
 			OrderReturnRequest req=new OrderReturnRequest();
 			req.setOrderId(Long.parseLong(orderId));
 			req.setProdDetalId(Long.parseLong(prodDetalId));
-			req.setTenantId("SLP");
+			req.setProdSum(Long.parseLong(prodSum));
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			req.setOperId("123");
 			IOrderAfterSaleSV orderAfterSaleSV = DubboConsumerFactory.getService(IOrderAfterSaleSV.class);
 			BaseResponse response = orderAfterSaleSV.back(req);
 			if(response.getResponseHeader().isSuccess()) {
 				data=new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "退货成功", null);
 			}else {
-				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, response.getResponseHeader().getResultMessage(), null);
+				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退货出错", null);
 			}
 		} catch (Exception e) {
-			LOG.error(e.getMessage());
+			LOG.info(e.getMessage());
 			data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退货失败,出现未知异常", null);
 		}
 		return data;
@@ -47,23 +53,25 @@ public class AfterSaleOrderController {
 	@RequestMapping("/exchange")
 	@ResponseBody
 	public ResponseData<String> exchange(HttpServletRequest request, String orderId,String prodDetalId) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<String> data=null;
 		try {
 			OrderReturnRequest req=new OrderReturnRequest();
 			req.setOrderId(Long.parseLong(orderId));
 			req.setProdDetalId(Long.parseLong(prodDetalId));
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			req.setOperId("123");
 			IOrderAfterSaleSV orderAfterSaleSV = DubboConsumerFactory.getService(IOrderAfterSaleSV.class);
 			BaseResponse response = orderAfterSaleSV.exchange(req);
 			if(response.getResponseHeader().isSuccess()) {
-				data=new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "退货成功", null);
+				data=new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "换货成功", null);
 			}else {
-				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, response.getResponseHeader().getResultMessage(), null);
+				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "换货出错", null);
 			}
 		} catch (Exception e) {
 			LOG.error(e.getMessage());
-			data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退货失败,出现未知异常", null);
+			data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "换货失败,出现未知异常", null);
 		}
 		return data;
 	}
@@ -71,23 +79,25 @@ public class AfterSaleOrderController {
 	@RequestMapping("/refund")
 	@ResponseBody
 	public ResponseData<String> refund(HttpServletRequest request, String orderId,String prodDetalId) {
+		GeneralSSOClientUser user = (GeneralSSOClientUser) request.getSession().getAttribute(SSOClientConstants.USER_SESSION_KEY);
 		ResponseData<String> data=null;
 		try {
 			OrderReturnRequest req=new OrderReturnRequest();
 			req.setOrderId(Long.parseLong(orderId));
 			req.setProdDetalId(Long.parseLong(prodDetalId));
-			req.setTenantId("SLP");
+			//req.setTenantId(user.getTenantId());
+			req.setTenantId("changhong");
 			req.setOperId("123");
 			IOrderAfterSaleSV orderAfterSaleSV = DubboConsumerFactory.getService(IOrderAfterSaleSV.class);
 			BaseResponse response = orderAfterSaleSV.refund(req);
 			if(response.getResponseHeader().isSuccess()) {
-				data=new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "退货成功", null);
+				data=new ResponseData<String>(ResponseData.AJAX_STATUS_SUCCESS, "退款成功", null);
 			}else {
-				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, response.getResponseHeader().getResultMessage(), null);
+				data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退款出错", null);
 			}
 		} catch (Exception e) {
 			LOG.error(e.getMessage());
-			data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退货失败,出现未知异常", null);
+			data=new ResponseData<String>(ResponseData.AJAX_STATUS_FAILURE, "退款失败,出现未知异常", null);
 		}
 		return data;
 	}
