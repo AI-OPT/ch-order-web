141d714542c906f195c42fc14ec99540ae4da7ef
fazhicao <409993207@qq.com>
2016-09-12 16:10:36 +0800

    test

--- src/main/java/com/ai/ch/order/web/controller/order/FramController.java
+++ src/main/java/com/ai/ch/order/web/controller/order/FramController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class FramControlller {
+public class FramController {
 	@RequestMapping("/frame")
 	public ModelAndView register(HttpServletRequest request) {
 
