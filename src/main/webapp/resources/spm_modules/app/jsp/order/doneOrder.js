d42f926d52c25291dbf8e961f5787baa728bbda7
fazhicao <409993207@qq.com>
2016-09-14 11:04:16 +0800

    已完成js

---
+++ src/main/webapp/resources/spm_modules/app/jsp/order/doneOrder.js
@@ -0,0 +1,178 @@
+define('app/jsp/order/doneOrder', function (require, exports, module) {
+    'use strict';
+    var $=require('jquery'),
+    Widget = require('arale-widget/1.2.0/widget'),
+    Dialog = require("artDialog/src/dialog"),
+    Paging = require('paging/0.0.1/paging-debug'),
+    AjaxController = require('opt-ajax/1.0.0/index');
+    require("jsviews/jsrender.min");
+    require("jsviews/jsviews.min");
+    require("bootstrap-paginator/bootstrap-paginator.min");
+    require("app/util/jsviews-ext");
+    
+    require("opt-paging/aiopt.pagination");
+    require("twbs-pagination/jquery.twbsPagination.min");
+    require('bootstrap/js/modal');
+    var SendMessageUtil = require("app/util/sendMessage");
+    
+    //实例化AJAX控制处理对象
+    var ajaxController = new AjaxController();
+    //定义页面组件类
+    var demopagePager = Widget.extend({
+    	
+    	Implements:SendMessageUtil,
+    	//属性，使用时由类的构造函数传入
+    	attrs: {
+    	},
+    	Statics: {
+    		DEFAULT_PAGE_SIZE: 10,
+    		USER_LEFT_MNU_ID: "left_mnu_account_balance"
+    	},
+    	//事件代理
+    	events: {
+    		//查询
+    		"click #backPage":"_back"
+    	},
+    	//重写父类
+    	setup: function () {
+    		demopagePager.superclass.setup.call(this);
+    	},
+    	
+    	
+    	_back:function() {
+    		//调到订单列表页面
+    		window.location.href = _base+"/order/toOrderList"
+    	},
+    	
+    	_backOrder:function(orderObject) {
+			 var _obj=$("#backNum"+orderObject).val();
+			 var _orderId = $('#orderId').val();
+			 var _prodDetalId=orderObject;
+			 ajaxController.ajax({
+					type : "POST",
+					url :_base+"/aftersaleorder/back",
+					data: {
+						orderId:  _orderId,
+						prodDetalId:_prodDetalId,
+						prodSum:_obj
+					},
+					processing: true,
+					message : "正在处理中，请稍候...",
+					success : function(data) {
+						if(data.statusCode == "1"){
+							var d = Dialog({
+								title: '消息',
+								content:"退货申请成功",
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}else{
+	    	        		var d = Dialog({
+								title: '消息',
+								content:"退货申请失败:"+data.statusInfo,
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}
+					}
+				});
+		 },
+		 
+		 _exchangeOrder:function(orderObject) {
+			 var _orderId = $('#orderId').val();
+			 var _prodDetalId=orderObject;
+			 ajaxController.ajax({
+					type : "POST",
+					url :_base+"/aftersaleorder/exchange",
+					data: {
+						orderId:  _orderId,
+						prodDetalId:_prodDetalId
+					},
+					processing: true,
+					message : "正在处理中，请稍候...",
+					success : function(data) {
+						if(data.statusCode == "1"){
+							var d = Dialog({
+								title: '消息',
+								content:"换货申请成功",
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}else{
+	    	        		var d = Dialog({
+								title: '消息',
+								content:"换货申请失败:"+data.statusInfo,
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}
+					}
+				});
+		 },
+		 
+		 _refundOrder:function(orderObject) {
+			 var _orderId = $('#orderId').val();
+			 var _prodDetalId=orderObject;
+			 ajaxController.ajax({
+					type : "POST",
+					url :_base+"/aftersaleorder/refund",
+					data: {
+						orderId:  _orderId,
+						prodDetalId:_prodDetalId
+					},
+					processing: true,
+					message : "正在处理中，请稍候...",
+					success : function(data) {
+						if(data.statusCode == "1"){
+							var d = Dialog({
+								title: '消息',
+								content:"退款申请成功",
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}else{
+	    	        		var d = Dialog({
+								title: '消息',
+								content:"退款申请失败:"+data.statusInfo,
+								icon:'prompt',
+								okValue: '确 定',
+								ok:function(){
+									this.close();
+								}
+							});
+							d.show();
+	    	        	}
+					}
+				});
+		 },
+		 
+		 _afterorderdetail:function(orderId,skuId){
+				window.location.href = _base+"/judge?orderId="
+	            + orderId+"&skuId="+skuId;
+			}
+    	
+    });
+    
+    module.exports = demopagePager
+});
+
