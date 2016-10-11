3676ad85314a07b283d31f365dec727de1dd73cb
zhangzhongde <xtgjzzd@163.com>
2016-10-11 17:08:17 +0800

    数据校验 订单监控规则

--- src/main/webapp/resources/spm_modules/app/jsp/order_rule/orderRuleSetting.js
+++ src/main/webapp/resources/spm_modules/app/jsp/order_rule/orderRuleSetting.js
@@ -13,6 +13,7 @@ define('app/jsp/order_rule/orderRuleSetting', function (require, exports, module
     require("opt-paging/aiopt.pagination");
     require("twbs-pagination/jquery.twbsPagination.min");
     require("bootstrap/js/modal");
+    require("jquery-validation/1.15.1/jquery.validate");
     var SendMessageUtil = require("app/util/sendMessage");
     
     //实例化AJAX控制处理对象
@@ -37,8 +38,117 @@ define('app/jsp/order_rule/orderRuleSetting', function (require, exports, module
     	setup: function () {
     		AddPager.superclass.setup.call(this);
     		this._findOrderRuleDetail();
+    		var formValidator=this._orderRuleSettingFormValidate();
+			$(":input").bind("focusout",function(){
+				formValidator.element(this);
+			});
+    	},
+    	//form校验
+    	_orderRuleSettingFormValidate:function(){
+    		//
+    		var orderRuleSettingFormValidator=$("#orderRuleSettingForm").validate({
+    			rules: {
+    				"command.timeMonitorTime": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.timeMonitorOrderSum": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.buyEmployeeMonitorTime": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.buyEmployeeMonitorOrderSum": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.buyIpMonitorTime": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.buyIpMonitorOrderSum": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				},
+    				"command.mergeOrderSettingTime": {
+    					required: true,
+    					digits:true,
+    					min:1,
+    					max:999999999
+    				}
+    				
+    			},
+    			messages: {
+    				"command.timeMonitorTime": {
+    					required: "请输入",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.timeMonitorOrderSum": {
+    					required: "请输入订单数量",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.buyEmployeeMonitorTime": {
+    					required: "请输入",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.buyEmployeeMonitorOrderSum": {
+    					required: "请输入订单数量",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.buyIpMonitorTime": {
+    					required: "请输入",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.buyIpMonitorOrderSum": {
+    					required: "请输入订单数量",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				},
+    				"command.mergeOrderSettingTime": {
+    					required: "请输入",
+    					digits: "只能输入数字",
+    					min:"最小值为{0}",
+    					max:"最大值为{0}"
+    				}
+    				
+    			}
+    		});
+    		
+    		return orderRuleSettingFormValidator;
     	},
     	_orderRuleSetting:function(){
+    		var _this = this;
+    		var formValidator=_this._orderRuleSettingFormValidate();
+			formValidator.form();
+			if(!$("#orderRuleSettingForm").valid()){
+				
+				return;
+			}
     		var data = $("#orderRuleSettingForm").serialize();
     		//alert(data);
     		ajaxController.ajax({
@@ -96,6 +206,14 @@ define('app/jsp/order_rule/orderRuleSetting', function (require, exports, module
 			);
     	},
     	_saveModalSure:function(){
+    		var _this = this;
+    		var formValidator=_this._orderRuleSettingFormValidate();
+			formValidator.form();
+			if(!$("#orderRuleSettingForm").valid()){
+				
+				return;
+			}
+			//
     		$('#saveModal').modal('show');
     	}
       	
