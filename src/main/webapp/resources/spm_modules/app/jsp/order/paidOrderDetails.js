dd373e089e9f6b4c45030fdc45fd6889d01b0c1d
fazhicao <409993207@qq.com>
2016-10-22 18:42:55 +0800

    添加数量

--- src/main/webapp/resources/spm_modules/app/jsp/order/paidOrderDetails.js
+++ src/main/webapp/resources/spm_modules/app/jsp/order/paidOrderDetails.js
@@ -173,8 +173,7 @@ define('app/jsp/order/paidOrderDetails', function (require, exports, module) {
 			 
 		 },
 		 
-		 _backOrder:function(orderObject) {
-			 var _obj=$("#backNum"+orderObject).val();
+		 _backOrder:function(orderObject,backSum) {
 			 var _orderId = $('#orderId').val();
 			 var _pOrderId = $('#pOrderId').val();
 			 var _state = $('#state').val();
@@ -185,7 +184,7 @@ define('app/jsp/order/paidOrderDetails', function (require, exports, module) {
 					data: {
 						orderId:  _orderId,
 						prodDetalId:_prodDetalId,
-						prodSum:_obj
+						prodSum:backSum
 					},
 					processing: true,
 					message : "正在处理中，请稍候...",
@@ -220,7 +219,6 @@ define('app/jsp/order/paidOrderDetails', function (require, exports, module) {
 		 },
 		 
 		 _refundOrder:function(orderObject,refundSum) {
-			 var _obj=$("#refundNum"+orderObject).val();
 			 var _orderId = $('#orderId').val();
 			 var _prodDetalId=orderObject;
 			 var _pOrderId = $('#pOrderId').val();
@@ -231,7 +229,7 @@ define('app/jsp/order/paidOrderDetails', function (require, exports, module) {
 					data: {
 						orderId:  _orderId,
 						prodDetalId:_prodDetalId,
-						prodSum:_obj
+						prodSum:refundSum
 					},
 					processing: true,
 					message : "正在处理中，请稍候...",
