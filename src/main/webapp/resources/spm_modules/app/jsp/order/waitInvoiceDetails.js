6b444acc539259407b83853dd0ddabd932f312c0
fazhicao <409993207@qq.com>
2016-11-09 12:13:43 +0800

    添加打印效果

--- src/main/webapp/resources/spm_modules/app/jsp/order/waitInvoiceDetails.js
+++ src/main/webapp/resources/spm_modules/app/jsp/order/waitInvoiceDetails.js
@@ -16,6 +16,8 @@ define('app/jsp/order/waitInvoiceDetails', function (require, exports, module) {
     require("opt-paging/aiopt.pagination");
     require("twbs-pagination/jquery.twbsPagination.min");
     require('bootstrap/js/modal');
+    require("jsviews/jquery.jqprint-0.3");
+    require("jsviews/jquery-migrate-1.1.0.min");
     var SendMessageUtil = require("app/util/sendMessage");
     
     //实例化AJAX控制处理对象
@@ -304,7 +306,18 @@ define('app/jsp/order/waitInvoiceDetails', function (require, exports, module) {
     	        }
                 
     	    });
-		}
+		},
+		
+		_truePrint:function(){
+	        $("#realPrint").jqprint(
+	        		 {
+	        		     debug: false, //如果是true则可以显示iframe查看效果（iframe默认高和宽都很小，可以再源码中调大），默认是false
+	        		     importCSS: true, //true表示引进原来的页面的css，默认是true。（如果是true，先会找$("link[media=print]")，若没有会去找$("link")中的css文件）
+	        		     printContainer: true, //表示如果原来选择的对象必须被纳入打印（注意：设置为false可能会打破你的CSS规则）。
+	        		     operaSupport: true//表示如果插件也必须支持歌opera浏览器，在这种情况下，它提供了建立一个临时的打印选项卡。默认是true
+	        		}	
+	        );
+	    }
     });
     
     module.exports = demopagePager
