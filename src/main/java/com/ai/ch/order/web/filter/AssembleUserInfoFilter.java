825594592c5bbcdab212d7f97f1f6b32301a6275
gucl <gucl@asiainfo.com>
2016-11-18 15:01:06 +0800

    403、404、500问题

--- src/main/java/com/ai/ch/order/web/filter/AssembleUserInfoFilter.java
+++ src/main/java/com/ai/ch/order/web/filter/AssembleUserInfoFilter.java
@@ -1,8 +1,11 @@
 package  com.ai.ch.order.web.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.Filter;
@@ -12,6 +15,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.jasig.cas.client.authentication.AttributePrincipal;
@@ -19,13 +23,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ai.ch.order.web.model.sso.client.GeneralSSOClientUser;
+import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.ai.opt.sso.client.filter.SSOClientConstants;
+import com.ai.platform.common.api.menu.interfaces.ISysMenuQuerySV;
+import com.ai.platform.common.api.menu.param.SysMenuListQueryRequest;
+import com.ai.platform.common.api.menu.param.SysMenuListQueryResponse;
 import com.alibaba.fastjson.JSON;
 
 
 public class AssembleUserInfoFilter implements Filter {
     private String[] ignor_suffix = {};
     private static final Logger LOG = LoggerFactory.getLogger(AssembleUserInfoFilter.class);
+	private static final String USER_MENUS = "user_menus";
 
     public void init(FilterConfig filterConfig) throws ServletException {
         String ignore_res = filterConfig.getInitParameter("ignore_suffix");
@@ -42,22 +51,47 @@ public class AssembleUserInfoFilter implements Filter {
             chain.doFilter(req, response);
             return;
         }
-        HttpSession session = req.getSession();
-        GeneralSSOClientUser user = (GeneralSSOClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
-        if (user == null) {
-            user = assembleUser(req);
-            if(user!=null){
-            	session.setAttribute(SSOClientConstants.USER_SESSION_KEY, user);
-            	LOG.info("已封装的用户信息为：" + JSON.toJSONString(user));
+        else{
+        	HttpSession session = req.getSession();
+            GeneralSSOClientUser user = (GeneralSSOClientUser) session.getAttribute(SSOClientConstants.USER_SESSION_KEY);
+            if (user == null) {
+                user = assembleUser(req);
+                if(user!=null){
+                	//用户信息存入session
+                	session.setAttribute(SSOClientConstants.USER_SESSION_KEY, user);
+                	
+                	//获取用户的菜单信息，存到session里面
+                	ISysMenuQuerySV menuSV=DubboConsumerFactory.getService(ISysMenuQuerySV.class);
+                	SysMenuListQueryRequest cond=new SysMenuListQueryRequest();
+                	cond.setUserId(user.getUserId());
+                	cond.setTenantId(user.getTenantId());
+                	SysMenuListQueryResponse menuResp=menuSV.queryMenuByUserId(cond);
+                	
+                	List<String> menuList=new ArrayList<String>();
+                	if(menuResp.getResponseHeader()!=null&&"000000".equalsIgnoreCase(menuResp.getResponseHeader().getResultCode())){
+                		menuList=menuResp.getMenuList();
+                		session.setAttribute(USER_MENUS, menuList);
+                	}
+                	
+                	LOG.info("已封装的用户信息为：" + JSON.toJSONString(user));
+                }
+                else{
+                	LOG.info("未获取到用户信息");
+                }            
+
+            } 
+            //判断权限 若果没有权限跳到403，判断规则  request.getRequestURI 去掉request.getContext前缀    
+            boolean authMenuFlag=authMenu(req);
+            if(!authMenuFlag){
+            	//((HttpServletResponse)response).setStatus(HttpServletResponse.SC_FORBIDDEN);
+            	((HttpServletResponse)response).sendRedirect(req.getContextPath()+"/403.jsp");
             }
             else{
-            	LOG.info("未获取到用户信息");
+            	chain.doFilter(req, response);
             }
-            chain.doFilter(req, response);
-
-        } else {
-            chain.doFilter(req, response);
         }
+        
+        
     }
 
     @Override
@@ -108,5 +142,28 @@ public class AssembleUserInfoFilter implements Filter {
             }
         }
         return true;
+    }
+    private boolean authMenu(HttpServletRequest request) {
+    	
+    	String currentURL = request.getRequestURI(); // 取得根目录所对应的绝对路径:
+    	LOG.debug("currentURL=" + currentURL);
+		String targetURL =currentURL.replace(request.getContextPath(), "");
+		LOG.debug("targetURL=" + targetURL);
+		List<String> menuList=new ArrayList<String>();
+		if(request.getSession().getAttribute(USER_MENUS)!=null){
+			menuList=(List<String>) request.getSession().getAttribute(USER_MENUS);			
+		}
+		//各中心自测的frame直接放行
+		if(targetURL.endsWith("/")||targetURL.endsWith("frame")||targetURL.endsWith("/frame.jsp")){
+			return true;
+		}
+    	if (menuList != null && menuList.size() > 0) {
+    		for (String menu : menuList) {
+    			if (menu.toLowerCase().trim().contains(targetURL.toLowerCase().trim())) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
     }
 }
