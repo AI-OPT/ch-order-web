5a449d325977e3741279c5736e0f06958ba161e2
fazhicao <409993207@qq.com>
2016-12-15 13:52:24 +0800

    取消注释

--- src/main/webapp/resources/template/default/scripts/plugin/bootstrap.js
+++ src/main/webapp/resources/template/default/scripts/plugin/bootstrap.js
@@ -338,7 +338,7 @@ Modal.prototype.checkScrollbar=function(){if(document.body.clientWidth>=window.i
 this.scrollbarWidth=this.scrollbarWidth||this.measureScrollbar()}
 Modal.prototype.setScrollbar=function(){var bodyPad=parseInt((this.$body.css('padding-right')||0),10)
 if(this.scrollbarWidth)this.$body.css('padding-right',bodyPad+ this.scrollbarWidth)}
-/*Modal.prototype.resetScrollbar=function(){this.$body.css('padding-right','')}*/
+Modal.prototype.resetScrollbar=function(){this.$body.css('padding-right','')}
 Modal.prototype.measureScrollbar=function(){var scrollDiv=document.createElement('div')
 scrollDiv.className='modal-scrollbar-measure'
 this.$body.append(scrollDiv)
