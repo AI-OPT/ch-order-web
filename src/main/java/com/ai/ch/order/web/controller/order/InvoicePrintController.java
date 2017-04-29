977948263af865e1386d012242c67bb66da157c1
fazhicao <409993207@qq.com>
2017-04-30 00:37:38 +0800

    冗余代码清除

--- src/main/java/com/ai/ch/order/web/controller/order/InvoicePrintController.java
+++ src/main/java/com/ai/ch/order/web/controller/order/InvoicePrintController.java
@@ -74,56 +74,12 @@ public class InvoicePrintController {
 			bodyList.add(invoicePrintInfo);
 		}
 		//
-//		String date_now = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-//		body.setCorporationCode("2000"); // 公司代码
-//		body.setInvoiceClass("0");// 发票类型 0 电子发票 1纸质发票
-//		body.setInvoiceKind("003"); // 发票种类 001增值税发票 002增值税普通电子发票 003普通发票
-//									// 004增值税普通发票 005增值税纸质发票 增值税电子发票
-//		body.setBuyerTaxpayerNumber("510798326942604"); // 购货方纳税人识别号
-//		body.setBuyerCode("");// 购货方代码
-//		body.setBuyerName("四川智易家网络科技有限公司"); // 购货方名称
-//		body.setBuyerAddress("四川绵阳高新区绵兴东路35号 "); // 购货方地址
-//		body.setBuyerTelephone("15802856879");// 购货方固定电话
-//		body.setBuyerMobiile("0816-2438114");// 购货方手机
-//		body.setBuyerEmail("519945018@qq.com");// 购货方邮箱
-//		body.setBuyerCompanyClass("03"); // 购货方类型 01:企业 02：机关事业单位 03：个人 04：其它
-//		
-//		body.setBuyerBankCode("10010");//购货方开户行代码
-//		body.setBuyerBankName("中国工商银行股份有限公司绵阳高新技术产业开发支行");//购货方开户行名称
-//		body.setBuyerBankAccount("2308414119100081746"); // 购货方银行账号
-//		
-//
-//		body.setSalesOrderNo("0000012"); // 销售订单号
-//		body.setOrderItem("123456789"); // 项目号
-//		body.setOrderCreateTime(""+DateUtil.getSysDate()); // （销售）订单创建日期
-//
-//		body.setMaterialName("43U1"); // 物料代码
-//		body.setSpecification("55q2n");
-//		body.setMaterialCode("1233333"); // 物料名称
-//		body.setPrice("2213.67666666667");
-//		body.setQuantity("3"); // 数量
-//		body.setUnit("台");// 物料单位
-//		body.setDiscountAmount("0.00"); // 折扣金额
-//		body.setRate("0.17"); // 税率
-//		body.setTax("1128.97"); // 税金
-//		body.setAmount("2213.67666666667"); // (单价)金额
-//		body.setTaxAmount("7770"); // 含税金额
-//		body.setRemark("打印发票请求");//备注
-		
-
-		
-		//获取授权ID
-		//body.setId(InvoiceUtils.getID(InvoiceUtils.TYPE_BATCH_ADD));//设置授权ID
-		
-		
-		//
 		ListInvoicePrintInfo listInvoicePrintInfo = new ListInvoicePrintInfo();
 		//
 		listInvoicePrintInfo.setList(bodyList);
 		listInvoicePrintInfo.setId(InvoiceUtils.getID(InvoiceUtils.TYPE_BATCH_ADD));
 		JSONObject invoicePrintJson =JSONObject.parseObject(JSONObject.toJSONString(listInvoicePrintInfo)); 
 		
-		//JSONArray invoicePrintJson = JSONArray.parseArray(JSONArray.toJSONString(bodyList));
 		LOG.info("bodyList:"+invoicePrintJson.toJSONString());
 		String retVal = InvoiceUtils.postBatchAdd(invoicePrintJson.toJSONString());
 		LOG.info("retVal:"+retVal);
